27dadddf34ee8bd2731f4cbb3ff412fecca8b1bb
Oussama Safi <104079090+oussamaSafi@users.noreply.github.com>
2023-05-03 23:54:09 +0100

    Update code_esp32.ino

--- code_esp32/code_esp32.ino
+++ code_esp32/code_esp32.ino
@@ -8,57 +8,43 @@
 #define RX_PIN 16
 #define TX_PIN 17
 
-#define WIFI_SSID "*******Secret***************"
-#define WIFI_PASSWORD "*******Secret**************"
+#define WIFI_SSID "azerty"
+#define WIFI_PASSWORD "123456789E"
 
-#define FIREBASE_HOST "***********Secret*****************"
-#define FIREBASE_AUTH "********Secret********************"
+#define FIREBASE_HOST "https://my-bus-final-default-rtdb.firebaseio.com/"
+#define FIREBASE_AUTH "AIzaSyAGkegz-UP_A015MVRaTy_bdzkWApNM7IY"
 
 SoftwareSerial gpsSerial(RX_PIN, TX_PIN);
 TinyGPSPlus gps;
 FirebaseData firebaseData;
 
-volatile bool gps_status=true;
+
 const int interruptPin = 15;
 unsigned long button_time = 0;  
 unsigned long last_button_time = 0; 
+
+bool gps_data_status= false;
+volatile bool gps_status=true;
+
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup() {
   lcd.init();                      // initialize the LCD
   lcd.backlight();
+  Display_info();
   gpsSerial.begin(9600);
   pinMode(interruptPin, INPUT);
   attachInterrupt(interruptPin, interruptHandler, FALLING);
   connectToWifi();
   connectToFirebase();
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Ready!");
   delay(1000);
-  lcd.clear();
-  lcd.setCursor(8,1);
-  lcd.print("GPS: ");
-  lcd.setCursor(13,1);
-  lcd.print("ON");
 }
 
 void loop() {
   
   while (gpsSerial.available() > 0) {
-    if(gps_status){
-    lcd.setCursor(8,1);
-    lcd.print("GPS: ");
-    lcd.setCursor(13,1);
-    lcd.print("ON");
-    }
-    else{
-    lcd.setCursor(8,1);
-    lcd.print("GPS: ");
-    lcd.setCursor(13,1);
-    lcd.print("OFF");
-    }
+    
     if (gps.encode(gpsSerial.read())) {
       float latitude, longitude;
       if (gps.location.isValid()) {
@@ -67,9 +53,12 @@ void loop() {
 
         if(gps_status){
           sendPositionToFirebase(latitude, longitude);
+        }else{
+          gps_data_status=false;
         }
       }
     }
+    Update_Display_info(gps_data_status,gps_status);
   }
   
   
@@ -79,35 +68,27 @@ void loop() {
 //function that connects the esp32 dev board to Wi-Fi
 
 void connectToWifi() {
+  lcd.setCursor(5, 0);
+  lcd.print("F");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Connecting ");
-  lcd.setCursor(0, 1);
-  lcd.print("to Wi-Fi...");
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
   }
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Wi-Fi connected");
+  lcd.setCursor(5, 0);
+  lcd.print("T");
 }
 
 //function that connects to firebase realtime database
 
 void connectToFirebase() {
+  lcd.setCursor(9, 1);
+  lcd.print("F");
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Connecting ");
-  lcd.setCursor(0, 1);
-  lcd.print("to Firebase...");
   while (!Firebase.ready()) {
     delay(1000);
   }
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Firebase connected");
+  lcd.setCursor(9, 1);
+  lcd.print("T");
 }
 
 //Function that sends latitude and longitude coordinates to firebase realtime database
@@ -119,15 +100,13 @@ void sendPositionToFirebase(float latitude, float longitude) {
   if (!Firebase.ready()) {
     connectToFirebase();
   }
-
-  Firebase.setFloat(firebaseData, "/Buses/Bus1/latitude", latitude);
-  Firebase.setFloat(firebaseData, "/Buses/Bus1/longitude", longitude);
-  lcd.setCursor(0,0);
-  lcd.print("coordinates ");
-  lcd.setCursor(0,1);
-  lcd.print(latitude, 3);
-  lcd.print("--");
-  lcd.print(longitude, 3); // display up to 3 decimal places
+  if(Firebase.setFloat(firebaseData, "/Buses/Bus1/latitude", latitude) && Firebase.setFloat(firebaseData, "/Buses/Bus1/longitude", longitude)){
+    gps_data_status = true;
+  }
+  else{
+    gps_data_status = false;
+  }
+  
 }
 
 //-------------interrupt------------------------------
@@ -138,4 +117,28 @@ void interruptHandler() {
     gps_status = !gps_status;
     last_button_time = button_time;
 }
+}
+
+
+void Update_Display_info(bool gps_data_status ,bool gps_status){
+  lcd.setCursor(15, 1);
+  if(gps_status){
+    lcd.print("T");
+  } else{
+    lcd.print("F");
+  }
+  lcd.setCursor(15, 0);
+  if(gps_data_status){
+    lcd.print("T");
+  } else{
+    lcd.print("F");
+  }
+}
+
+void Display_info(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("WiFi:F    Data:-");
+  lcd.setCursor(0, 1);
+  lcd.print("Firebase:F GPS:T");
 }
